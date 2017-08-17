193801eff05edf39a7dc6b94daedb488c8384054
AkshayHere <akshymohn@gmail.com>
2017-08-17 22:26:18 +0800

    Section C done

--- CSExercises/SectionC/ExC6.cs
+++ CSExercises/SectionC/ExC6.cs
@@ -30,17 +30,64 @@ namespace CSExercises
         public static void Main(string[] args)
         {
             //YOUR CODE HERE - get user input, call the function and return the discounted price
+            string code = null;
+            int tvNo = 0, DVDNo = 0, mp3No = 0;
 
+            do
+            {
+                Console.WriteLine("Enter product code \n 1. TV \n 2. DVD \n 3. MP3 \n 4. Exit");
+                code = Console.ReadLine();
+                
+                switch (code)
+                {
+                    case "1":
+                        Console.WriteLine("Enter the no of TVs");
+                        tvNo = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case "2":
+                        Console.WriteLine("Enter the no of DVDs");
+                        DVDNo = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case "3":
+                        Console.WriteLine("Enter the no of Mp3s");
+                        mp3No = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case "4":
+                        code = null;
+                        break;
+                }
+            } while (code != null);
 
+            double totalObtained = CalculateTotalPrice(tvNo, DVDNo, mp3No);
+            Console.WriteLine("Your order is as follows \n TVs:"+ tvNo +" \n DVDs: "+ DVDNo+" \n Mp3s: "+ mp3No+"");
+            Console.WriteLine("Total cost after discount is "+ totalObtained);
 
+            Console.ReadLine();
         }
 
         public static double CalculateTotalPrice(int tvQty, int dvdQty, int mp3Qty)
         {
             //YOUR CODE HERE
-            return 0;
 
+            int tvTotal = 900 * tvQty;
+            int dvdTotal = 500 * dvdQty;
+            int mp3Total = 700 * mp3Qty;
+            int discount = 0;
 
+            int total = tvTotal + dvdTotal + mp3Total;
+
+            if ((tvTotal + dvdTotal) >= 5000)
+            {
+                discount = (10 * total) / 100;
+            }
+            else if ((tvTotal + dvdTotal) >= 10000)
+            {
+                discount = (15 * total) / 100;
+            }
+
+            double revisedTotal = total - discount;
+
+            return revisedTotal;
 
         }
     }
